2b1545d824ff85ac0e1123e661720d9d47616fa2
PIYOKICHI\Kihiro <kihiro.nico25@gmail.com>
2018-05-10 18:10:38 +0900

    メソッド　引数2　課題提出いたします。 ご確認の程、お願いいたします

---
+++ CampChallenge_java/011.メソッド/Mezotto3.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.Mezotto.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+public class Mezotto3 extends HttpServlet {
+
+  
+
+void kakeru2(int num, int num2, boolean num3, PrintWriter pw) {
+    
+if (num3) {
+    pw.print(num*num*num2*num2);
+    }
+}
+void kakeru2(PrintWriter pw) {
+    kakeru2(5, 20, false, pw);
+}  
+    
+    
+    
+    
+ protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+      
+    
+    kakeru2( 20, 50, true , out);
+
+    kakeru2(out);
+ 
+}
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+        }
+}
