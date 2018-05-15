f7d78f7ec1671a8ebbf53108c6467d13f8e89805
PIYOKICHI\Kihiro <kihiro.nico25@gmail.com>
2018-05-15 11:37:15 +0900

    メソッド　戻り値2　課題提出いたします。 ご確認の程、お願いいたします

---
+++ CampChallenge_java/011.メソッド/Mezotto5.java
@@ -0,0 +1,95 @@
+
+package org.Mezotto.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+
+public class Mezotto5 extends HttpServlet {
+
+    
+private static String[] test(){
+  String data[] = new String[3];
+  data[0] = "0000";
+  data[1] = "2000/2/2";
+  data[2] = "東京";
+  
+  
+  return data;
+}    
+
+
+
+ 
+
+
+protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();    
+
+
+String data[];
+
+  data = test();
+  out.print(data[1]);
+  out.print(data[2]);
+}
+
+
+
+
+ 
+
+
+
+
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+
+}
