32628179e00763412bbeda2e133397fc0647dedb
PIYOKICHI\Kihiro <kihiro.nico25@gmail.com>
2018-05-15 20:49:16 +0900

    標準クラス　文字数の取得　課題提出いたします。 ご確認の程、お願いいたします

---
+++ CampChallenge_java/015.標準クラス/mozi2.java
@@ -0,0 +1,11 @@
+
+package org.standard.servlet;
+
+class mozi2 {
+    public static void main(String[] args) {
+        String mail = "kihirorororororor@yhooooooo.co.jp";
+
+        // @以降の文字数を表示 -- 17文字以降
+        System.out.println(mail.substring(17).length());
+    }
+}
